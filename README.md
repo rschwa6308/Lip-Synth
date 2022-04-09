6f64a17310726a29dbdf53676a7bbdc6f69624a2
Russell Schwartz <russell.w.schwartz@gmail.com>
2022-04-08 20:49:51 -0400

    restructure for express server

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bitcamp2022
+Russell Schwartz and Ryan Druffel
+
+## Usage
+To run the dev server, first install required packages
+
+```bash
+npm i
+```
+
+Then start the server
+
+```bash
+npm start
+```
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const { get } = require('request')
+
+// initialize the express app
+const app = express()
+
+// configure server
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// point server to static resources
+app.use(express.static(path.join(__dirname, './public')))
+
+// define url paths
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "index.html")))
+
+// listen on port 3000
+app.listen(3000, () => console.log('Listening on port 3000!'))
