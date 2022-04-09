25928f8be60f522e88cdeb77f91858d8ccfc37bc
Ryan Druffel <thefriendlynextdoorneighbor@gmail.com>
2022-04-08 20:25:55 -0400

    Initial webcam and website stuff.

---
+++ webcam.js
@@ -0,0 +1,22 @@
+// Delay collection of video element until after its been loaded
+document.addEventListener("DOMContentLoaded", function(){
+    videoElem = document.getElementById("videoElement");
+    console.log(videoElem)
+    getMediaDevice(videoElem)
+});
+
+function getMediaDevice (applyTo) {
+    // If media device is available
+    if (navigator.mediaDevices.getUserMedia) {
+        // Get media feed
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                // Apply video to video element
+                applyTo.srcObject = stream;
+            })
+            .catch(function (err) {
+                // Report an error if something went wrong
+                console.log("Something went wrong!\n" + err)
+            })
+    }
+}
